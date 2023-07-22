4529ae9a2c5aacc22a8537290251913da2297db2
geedtd <grcazares@gmail.com>
2023-07-22 13:26:14 -0700

    creating logic for logoDecal or FullTexture

--- client/src/canvas/Backdrop.jsx
+++ client/src/canvas/Backdrop.jsx
@@ -1,6 +1,6 @@
 import { useRef } from "react"
-import { easing } from "maath"
-import { useFrame } from "@react-three/fiber"
+// import { easing } from "maath"
+// import { useFrame } from "@react-three/fiber"
 import { AccumulativeShadows,RandomizedLight } from "@react-three/drei"
 
 const Backdrop = () => {
